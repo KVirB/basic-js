0283275a71e7ed2a1cae086746172f672ae2078e
KVirB <dlaydrygaakk@mail.ru>
2023-12-25 18:52:54 +0300

    feat: sort and sum complete

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let sort = arr.filter((item) => item !== -1).sort((a, b) => a - b);
+  let res = arr.map((item) => (item === -1 ? item : sort.shift()));
+  return res;
 }
-
+arr = [-1, 150, 190, 170, -1, -1, 160, 180];
+console.log(arr.filter((item) => item !== -1).sort((a, b) => a - b));
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
